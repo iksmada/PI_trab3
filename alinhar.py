6c3a62223ff99bac66502137ebc886b8f0f79ebb
Raphael Adamski <raphaeladamski@hotmail.com>
2018-05-12 17:28:59 -0300

    basic code

--- alinhar.py
+++ alinhar.py
@@ -1,11 +1,12 @@
 import argparse
+import cv2
 
-
+modes = ("projection", "hough")
 parser = argparse.ArgumentParser(description='Fix tilted images')
 parser.add_argument('-i', '--input', type=int, help='input image', required=True)
 parser.add_argument('-o', '--output', type=int, help='Output image name', required=True)
 parser.add_argument('-m', '--mode', type=str, help='Technique for alignment algorithm',
-                    default='projection', choices=("projection", "hough"))
+                    default='projection', choices=modes)
 
 args = vars(parser.parse_args())
 print(args)
@@ -16,5 +17,24 @@ MODE = args["mode"]
 if not OUTPUT.endswith(".png"):
     OUTPUT = OUTPUT + ".png"
 
+img_orig = cv2.imread(INPUT)
+img_greyscale = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
+
+
+def projection(img):
+    return 0
+
+
+def hough(img):
+    return 0
+
+
+angle = 361
+if MODE == modes[0]:
+    angle = projection(img_greyscale)
+elif MODE == modes[1]:
+    angle = hough(img_greyscale)
+
+print("Angulo de inclinação no sentido anti-horário: %d" % angle)
 
 
